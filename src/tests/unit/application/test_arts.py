d37646c4b1bbf75d04e774cdf957976378ba3d60
podrivnick <ribakov1973@gmail.com>
2024-09-08 14:47:24 +0300

    feat: Test Application Invalid Data.

--- src/tests/unit/application/test_arts.py
+++ src/tests/unit/application/test_arts.py
@@ -1,6 +1,9 @@
 import pytest
 from faker import Faker
-from fastapi import FastAPI
+from fastapi import (
+    FastAPI,
+    status,
+)
 from fastapi.testclient import TestClient
 from httpx import Response
 
@@ -19,3 +22,19 @@ async def test_get_random_art_success(
     json_data = response.json()
 
     assert json_data["result"][0]["art_direction"]["value"] == art_direction
+
+
+@pytest.mark.asyncio
+async def test_get_random_art_is_valid(
+    app: FastAPI,
+    client: TestClient,
+    faker: Faker,
+):
+    url = app.url_path_for("get_random_art_handler")
+    art_direction = "modern" * 50
+    response: Response = client.post(url=url, json={"art_direction": art_direction})
+
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
+    json_data = response.json()
+
+    assert json_data["detail"]["error"]
--- src/tests/unit/application/test_poems.py
+++ src/tests/unit/application/test_poems.py
@@ -1,6 +1,9 @@
 import pytest
 from faker import Faker
-from fastapi import FastAPI
+from fastapi import (
+    FastAPI,
+    status,
+)
 from fastapi.testclient import TestClient
 from httpx import Response
 
@@ -15,7 +18,23 @@ async def test_get_random_poem_success(
     poem_author = "George Orwell"
     response: Response = client.post(url=url, json={"poem_author": poem_author})
 
-    assert response.status_code == 201
+    assert response.status_code == status.HTTP_201_CREATED
     json_data = response.json()
 
     assert len(json_data["result"][0]["poem_author"]["value"]) > 0
+
+
+@pytest.mark.asyncio
+async def test_get_random_poem_is_valid(
+    app: FastAPI,
+    client: TestClient,
+    faker: Faker,
+):
+    url = app.url_path_for("get_random_poem_handler")
+    poem_author = "George Orwell" * 50
+    response: Response = client.post(url=url, json={"poem_author": poem_author})
+
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
+    json_data = response.json()
+
+    assert json_data["detail"]["error"]
